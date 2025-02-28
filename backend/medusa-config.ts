9bf65d8d1f923afdaa0c16a57c1f1a48b66b7c1a
ibreathe247365 <76894659+ibreathe247365@users.noreply.github.com>
2025-02-28 17:13:52 +0000

    bro work pls

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv, Modules } from "@medusajs/framework/utils";
 
 loadEnv(process.env.NODE_ENV, process.cwd());
 
-// const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 // const DB_NAME = process.env.DB_NAME;
 
 
@@ -12,9 +12,9 @@ const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_DATABASE = process.env.DB_DATABASE;
 
-const DATABASE_URL =
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+// const DATABASE_URL =
+//   `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
+//   `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 // const FINAL_DATABASE_URL =
 //   `${DATABASE_URL}/${DB_NAME}`;
